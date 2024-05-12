324922513612d34a53158a0bca591d3badf1f3ee
Adibkhan619 <work.adibkhan@gmail.com>
2024-05-12 12:09:33 +0600

    Added remove food added by user

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ async function run() {
             res.send(result)
         })
 
+        // DELETE FOOD ITEM ADDED BY USER
+        app.delete('/foods/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.deleteOne(query)
+            res.send(result)
+          })
 
         // ADD ORDER FROM USER
         app.post("/orders", async(req, res) => {
