76f69c11d9c6d8bbbaa8041f717a9db184ef5ee6
Adibkhan619 <work.adibkhan@gmail.com>
2024-05-12 11:55:44 +0600

    Added delete order by ID

--- index.js
+++ index.js
@@ -65,16 +65,17 @@ async function run() {
             res.send(result)
         })
 
+
         // ADD ORDER FROM USER
         app.post("/orders", async(req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order)
-            const updateOrder = {
-                $inc: { order_count: 1}
-            }
-            const orderQuery = { _id: new ObjectId(order.id)}
-            const updateOrderCount = await foodCollection.updateOne( orderQuery, updateOrder)
-            console.log(updateOrderCount);
+            // const updateOrder = {
+            //     $inc: { order_count: 1}
+            // }
+            // const orderQuery = { _id: new ObjectId(order.id)}
+            // const updateOrderCount = await foodCollection.updateOne( orderQuery, updateOrder)
+            // console.log(updateOrderCount);
             res.send(result);
         })
 
@@ -83,6 +84,22 @@ async function run() {
             const result = await orderCollection.find().toArray();
             res.send(result);
         })
+        
+        // FIND ORDER DATA WITH ID
+        app.get("/orders/:id", async(req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await orderCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // DELETE ORDERED FOOD
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+          })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
